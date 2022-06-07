9c6eb9fccb45e0f2f04c8d7272791d350064fa1d
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-07 11:15:21 -0400

    Connected listener to PORT, finishing initial server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 /////////////////////////////////////////////
 //! Import Our Dependencies
 /////////////////////////////////////////////
-require("dotenv").config(); // Load ENV Variables
-const express = require("express"); // import express
-const morgan = require("morgan"); //import morgan
-const methodOverride = require("method-override");
-const mongoose = require("mongoose");
-const path = require("path")
-
+require('dotenv').config(); // Load ENV Variables
+const express = require('express'); // import express
+const morgan = require('morgan'); //import morgan
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const path = require('path');
 
 /////////////////////////////////////////////
 //! Database Connection
@@ -24,10 +23,9 @@ mongoose.connect(DATABASE_URL, CONFIG);
 
 //? Events for when connection opens/disconnects/errors
 mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
-
+  .on('open', () => console.log('Connected to Mongoose'))
+  .on('close', () => console.log('Disconnected from Mongoose'))
+  .on('error', (error) => console.log(error));
 
 ////////////////////////////////////////////////
 //! Our Models
@@ -39,22 +37,36 @@ const { Schema, model } = mongoose;
 const fruitsSchema = new Schema({
   name: String,
   color: String,
- readyToEat: Boolean
+  readyToEat: Boolean,
 });
 
 //? make fruit model
-const Fruit = model("Fruit", fruitsSchema);
-
+const Fruit = model('Fruit', fruitsSchema);
 
 /////////////////////////////////////////////////
 //! Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
-const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+const app = require('liquid-express-views')(express(), {
+  root: [path.resolve(__dirname, 'views/')],
+});
 
 /////////////////////////////////////////////////////
 //! Middleware
 /////////////////////////////////////////////////////
-app.use(morgan("dev")); //? logging
-app.use(methodOverride("_method")); //? override for put and delete requests from forms
+app.use(morgan('dev')); //? logging
+app.use(methodOverride('_method')); //? override for put and delete requests from forms
 app.use(express.urlencoded({ extended: true })); //? parse urlencoded request bodies
-app.use(express.static("public")); //? serve files from public statically
+app.use(express.static('public')); //? serve files from public statically
+
+////////////////////////////////////////////
+//! Routes
+////////////////////////////////////////////
+app.get('/', (req, res) => {
+  res.send('your server is running... better catch it.');
+});
+
+//////////////////////////////////////////////
+//! Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
