a27d82590dd384aa311b7437d268468a09892afd
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-07 11:12:27 -0400

    Added middlewares and created app object

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const path = require("path")
 /////////////////////////////////////////////
 //! Database Connection
 /////////////////////////////////////////////
-// Setup inputs for our connect function
+//? Setup inputs for our connect function
 const DATABASE_URL = process.env.DATABASE_URL;
 const CONFIG = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// Establish Connection
+//? Establish Connection
 mongoose.connect(DATABASE_URL, CONFIG);
 
-// Events for when connection opens/disconnects/errors
+//? Events for when connection opens/disconnects/errors
 mongoose.connection
   .on("open", () => console.log("Connected to Mongoose"))
   .on("close", () => console.log("Disconnected from Mongoose"))
@@ -32,15 +32,29 @@ mongoose.connection
 ////////////////////////////////////////////////
 //! Our Models
 ////////////////////////////////////////////////
-// pull schema and model from mongoose
+//? pull schema and model from mongoose
 const { Schema, model } = mongoose;
 
-// make fruits schema
+//? make fruits schema
 const fruitsSchema = new Schema({
   name: String,
   color: String,
  readyToEat: Boolean
 });
 
-// make fruit model
+//? make fruit model
 const Fruit = model("Fruit", fruitsSchema);
+
+
+/////////////////////////////////////////////////
+//! Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+
+/////////////////////////////////////////////////////
+//! Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("dev")); //? logging
+app.use(methodOverride("_method")); //? override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); //? parse urlencoded request bodies
+app.use(express.static("public")); //? serve files from public statically
